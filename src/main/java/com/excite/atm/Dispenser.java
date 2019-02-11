71f2d6d43212a140e40a27b337d5d7e4a7f60de9
vchatchai <ee56054@gmail.com>
2019-02-11 22:52:17 +0700

    add ui

--- src/main/java/com/excite/atm/Dispenser.java
+++ src/main/java/com/excite/atm/Dispenser.java
@@ -1,186 +1,224 @@
 package com.excite.atm;
 
 import java.math.BigDecimal;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class Dispenser {
-	Map<NoteType, Integer> currentMoney = new HashMap<>();
 
-	public BigDecimal deposit(Map<NoteType, Integer> amount) {
+    Map<NoteType, Integer> currentMoney = new HashMap<>();
 
-		currentMoney.putAll(amount);
-		BigDecimal totalAmount = getTotalMoney(amount);
+    public BigDecimal deposit(Map<NoteType, Integer> amount) {
 
-		return totalAmount;
-	}
+        for (Map.Entry<NoteType, Integer> entry : amount.entrySet()) {
+            Integer value = currentMoney.get(entry.getKey());
+            if (Objects.isNull(value)) {
+                value = 0;
+            }
 
-	public BigDecimal getTotalMoney(Map<NoteType, Integer> amount) {
-		BigDecimal totalAmount = amount.entrySet().stream()
-				.map(money -> money.getKey().getAmount().multiply(new BigDecimal(money.getValue())))
-				.reduce(BigDecimal.ZERO, BigDecimal::add);
-		return totalAmount;
-	}
+            value = value + entry.getValue();
 
-	public Map<NoteType, Integer> dispensing(BigDecimal amount) throws MoneyException {
+            currentMoney.put(entry.getKey(), value);
+        }
 
-		BigDecimal totalCurrentMoney = getTotalMoney(currentMoney);
+        BigDecimal totalAmount = getTotalMoney(amount);
 
-		if (totalCurrentMoney.compareTo(amount) < 0) {
-			throw new MoneyException("Money is not enough for withdraw.");
-		}
+        return totalAmount;
+    }
 
-		Map<NoteType, Integer> result = new HashMap<>();
-		processing(amount, result, null);
-		
+    public String printMoney() {
+        StringBuilder builder = new StringBuilder();
 
-		BigDecimal totalWithdraw = getTotalMoney(result);
-		if (totalWithdraw.compareTo(amount) < 0) {
-			throw new MoneyException("Can't withdraw in this amount.");
-		}
+        Formatter fmt = new Formatter(builder);
+        fmt.format("Total Notes Remaining%n");
+        for (Map.Entry<NoteType, Integer> entry : currentMoney.entrySet()) {
+            if(entry.getValue() != 0){
+                fmt.format("%s = %s%n", entry.getKey().toString(), entry.getValue());
+            }
+        }
+        
+        fmt.format("Total Amount Remaining: %s%n", getTotalMoney());
+        return builder.toString();
+    }
 
-		return result;
-	}
+    public BigDecimal getTotalMoney() {
+        return getTotalMoney(currentMoney);
+    }
 
-	private void processing(BigDecimal withdraw, Map<NoteType, Integer> result,NoteType noteBefore ) {
-		BigDecimal amount = withdraw;
-		for (NoteType note : NoteType.values()) {
-			if(note.getAmount().compareTo(amount) < 0){
-				
-				Long total = amount.longValue()/note.getAmount().longValue();
-				for( ;total>0; total--) {
-					Integer money =	getMoney(currentMoney,note);
-					pushMoney(result, note, money);
-					
-				}
-				amount = withdraw.subtract(getTotalMoney(result));
-				
-			}
-			if(note.getAmount().compareTo(amount) == 0){
-		
-				
-				if(NoteType.Note20.equals(note)) {
-					Integer money =	getMoney(currentMoney,note);
-					pushMoney(result, note, money);
-					amount = getTotalMoney(result);
-					return;
-				}else if(!result.isEmpty()) {
+    public Map<NoteType, Integer> dispensing(BigDecimal amount) throws MoneyException {
 
-					Integer money =	getMoney(currentMoney,note);
-					pushMoney(result, note, money);
-					amount = getTotalMoney(result);
-					return;
-				}else if(NoteType.Note50.equals(note)) {
-					Integer money =	getMoney(currentMoney,note);
-					pushMoney(result, note, money);
-					amount = getTotalMoney(result);
-					return;
-				}
-				
-			} 
-			if(NoteType.Note20.equals(note)  && amount.longValue()< 20 && amount.longValue() > 0) {
-				
+        BigDecimal totalCurrentMoney = getTotalMoney(currentMoney);
 
-				Integer money =	getMoney(currentMoney,NoteType.Note50);
-				pushMoney(result, NoteType.Note50, money);
-				BigDecimal overAmount = getTotalMoney(result);
-				overAmount = overAmount.subtract(withdraw);
-				
-				if(overAmount.longValue()%NoteType.Note100.getAmount().longValue() == 0) {
-					
-					while(BigDecimal.ZERO.compareTo(overAmount) != 0) {
+        if (totalCurrentMoney.compareTo(amount) < 0) {
+            throw new MoneyException("Money is not enough for withdraw.");
+        }
 
-						money =	getMoney(result,NoteType.Note100);
-						if(money == 0) {
-							exchange(NoteType.Note100,result,currentMoney);
-						}
-						pushMoney(currentMoney, NoteType.Note100, money);
-						overAmount = getTotalMoney(result);
-						overAmount = overAmount.subtract(withdraw);
-					}
-					}
-				
-				if(overAmount.longValue()%NoteType.Note20.getAmount().longValue() == 0) {
-					while(BigDecimal.ZERO.compareTo(overAmount) != 0) {
+        Map<NoteType, Integer> result = new HashMap<>();
+        processing(amount, result, null);
 
-						money =	getMoney(result,NoteType.Note20);
-						if(money == 0) {
-							exchange(NoteType.Note20,result,currentMoney);
-						}
-						pushMoney(currentMoney, NoteType.Note20, money);
-						overAmount = getTotalMoney(result);
-						overAmount = overAmount.subtract(withdraw);
-					}	
-				}
-			}
-			
-		}	
+        BigDecimal totalWithdraw = getTotalMoney(result);
+        if (totalWithdraw.compareTo(amount) < 0) {
+            for(Map.Entry<NoteType,Integer> entry: result.entrySet()){
+                Integer note = currentMoney.get(entry.getKey());
+                if(Objects.isNull(note)){
+                    note = 0;
+                }
+                note = note + entry.getValue();
+                currentMoney.put(entry.getKey(), note);
+                entry.setValue(0);
+            }
 
-	}
-	
-	private void exchange(NoteType note, Map<NoteType, Integer> result, Map<NoteType, Integer> currentMoney) {
-		
-		
-		for(NoteType exchangeNote: NoteType.values()) {
-			if( !exchangeNote.equals(note) ) {
-				Integer totalNote = result.get(exchangeNote);
-				if(Objects.nonNull(totalNote) && totalNote > 0) {
-					for(int count = 1; count <= totalNote; count++) {
-						long exchangeNoteCount = exchangeNote.getAmount().longValue()*count;
-						if(exchangeNoteCount%note.getAmount().longValue() == 0) {
-					getMoney(currentMoney,note);
-					int total = (int)(exchangeNoteCount/note.getAmount().longValue());
-					pushMoney(result, note, total );
+            for(Map.Entry<NoteType,Integer> entry: currentMoney.entrySet()){
 
-					Integer money =	getMoney(result,exchangeNote,count);
-					pushMoney(currentMoney, exchangeNote, money);
-						
-						}
-					}
-				}
-				
-				
-				
-					
-					
-			}
-		}
-		
-		
-		
-	}
+                if(entry.getKey().getAmount().compareTo(amount) == 0){
+                   pushMoney(result,entry.getKey(), getMoney(currentMoney, entry.getKey()));
+                   return result;
+                }
 
-	private void pushMoney(Map<NoteType, Integer> result,NoteType note, Integer money) {
-		Integer temp = result.get(note);
-		if(Objects.isNull(temp)) {
-			temp = 0;
-		}
-		result.put(note, temp+money);
-		
-	}
-	private int getMoney(Map<NoteType, Integer> result, NoteType note){
-		return getMoney(result, note,1 );
-		
-		
-	}
-	
-	
-	private int getMoney(Map<NoteType, Integer> result, NoteType note, int count){
-		Integer money = result.get(note);
-		if(Objects.isNull(money)) {
-			return 0;
-		}
-		if(money == 0) {
-			return 0;
-		}else {
-			money = money -count;
-			result.put(note, money);
-			return count ;
-		}
-		
-		
-	}
-	
-	
+            }
+
+
+            throw new MoneyException("Can't withdraw in this amount.");
+        }
+
+        return result;
+    }
+
+    private BigDecimal getTotalMoney(Map<NoteType, Integer> amount) {
+        BigDecimal totalAmount = amount.entrySet().stream()
+                .map(money -> money.getKey().getAmount().multiply(new BigDecimal(money.getValue())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return totalAmount;
+    }
+
+    private void processing(BigDecimal withdraw, Map<NoteType, Integer> result, NoteType noteBefore) {
+        BigDecimal amount = withdraw;
+        for (NoteType note : NoteType.values()) {
+            if (note.getAmount().compareTo(amount) < 0) {
+
+                Long total = amount.longValue() / note.getAmount().longValue();
+                for (; total > 0; total--) {
+                    Integer money = getMoney(currentMoney, note);
+                    pushMoney(result, note, money);
+
+                }
+                amount = withdraw.subtract(getTotalMoney(result));
+
+            }
+            if (note.getAmount().compareTo(amount) == 0) {
+
+                if (NoteType.Note20.equals(note)) {
+                    Integer money = getMoney(currentMoney, note);
+                    pushMoney(result, note, money);
+                    amount = getTotalMoney(result);
+                    return;
+                } else if (!result.isEmpty()) {
+
+                    Integer money = getMoney(currentMoney, note);
+                    pushMoney(result, note, money);
+                    amount = getTotalMoney(result);
+                    return;
+                } else if (NoteType.Note50.equals(note)) {
+                    Integer money = getMoney(currentMoney, note);
+                    pushMoney(result, note, money);
+                    amount = getTotalMoney(result);
+                    return;
+                }
+
+            }
+            if (NoteType.Note20.equals(note) && amount.longValue() < 20 && amount.longValue() > 0) {
+
+                Integer money = getMoney(currentMoney, NoteType.Note50);
+                pushMoney(result, NoteType.Note50, money);
+                BigDecimal overAmount = getTotalMoney(result);
+                overAmount = overAmount.subtract(withdraw);
+
+                if (overAmount.longValue() % NoteType.Note100.getAmount().longValue() == 0) {
+
+                    while (BigDecimal.ZERO.compareTo(overAmount) != 0) {
+
+                        money = getMoney(result, NoteType.Note100);
+                        if (money == 0) {
+                            exchange(NoteType.Note100, result, currentMoney);
+                        }
+                        pushMoney(currentMoney, NoteType.Note100, money);
+                        overAmount = getTotalMoney(result);
+                        overAmount = overAmount.subtract(withdraw);
+                    }
+                }
+
+                if (overAmount.longValue() % NoteType.Note20.getAmount().longValue() == 0) {
+                    while (BigDecimal.ZERO.compareTo(overAmount) != 0) {
+
+                        money = getMoney(result, NoteType.Note20);
+                        if (money == 0) {
+                            exchange(NoteType.Note20, result, currentMoney);
+                        }
+                        pushMoney(currentMoney, NoteType.Note20, money);
+                        overAmount = getTotalMoney(result);
+                        overAmount = overAmount.subtract(withdraw);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private void exchange(NoteType note, Map<NoteType, Integer> result, Map<NoteType, Integer> currentMoney) {
+
+        for (NoteType exchangeNote : NoteType.values()) {
+            if (!exchangeNote.equals(note)) {
+                Integer totalNote = result.get(exchangeNote);
+                if (Objects.nonNull(totalNote) && totalNote > 0) {
+                    for (int count = 1; count <= totalNote; count++) {
+                        long exchangeNoteCount = exchangeNote.getAmount().longValue() * count;
+                        if (exchangeNoteCount % note.getAmount().longValue() == 0) {
+                            int total = (int) (exchangeNoteCount / note.getAmount().longValue());
+                            getMoney(currentMoney, note,total);
+                            pushMoney(result, note, total);
+
+                            Integer money = getMoney(result, exchangeNote, count);
+                            pushMoney(currentMoney, exchangeNote, money);
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private void pushMoney(Map<NoteType, Integer> result, NoteType note, Integer money) {
+        Integer temp = result.get(note);
+        if (Objects.isNull(temp)) {
+            temp = 0;
+        }
+        result.put(note, temp + money);
+
+    }
+
+    private int getMoney(Map<NoteType, Integer> result, NoteType note) {
+        return getMoney(result, note, 1);
+
+    }
+
+    private int getMoney(Map<NoteType, Integer> result, NoteType note, int count) {
+        Integer money = result.get(note);
+        if (Objects.isNull(money)) {
+            return 0;
+        }
+        if (money == 0) {
+            return 0;
+        } else {
+            money = money - count;
+            result.put(note, money);
+            return count;
+        }
+
+    }
+
 }
--- src/test/java/com/excite/atm/DispenserTest.java
+++ src/test/java/com/excite/atm/DispenserTest.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,27 +14,64 @@ import org.junit.jupiter.api.Test;
 class DispenserTest {
 
 	private Dispenser dispenser = new Dispenser();
+	private int Note1000 = 200;
+	private int Note500 = 200;
+	private int Note100 = 500;
+	private int Note50 = 1000;
+	private int Note20 = 5000;
 
 	@BeforeEach
 	public void setup() {
-		Map<NoteType, Integer> map = new HashMap<>();
-		map.put(NoteType.Note1000, 200);
-		map.put(NoteType.Note500, 200);
-		map.put(NoteType.Note100, 500);
-		map.put(NoteType.Note50, 1000);
-		map.put(NoteType.Note20, 5000);
-
-		BigDecimal amount = dispenser.deposit(map);
-		System.out.println("Deposit:" + amount);
 
 	}
 
 	@Test
-	public void testDispensing1000() throws MoneyException {
+	public void testDispensingUnderValue() throws MoneyException {
+		Map<NoteType, Integer> map = new HashMap<>();
+		map.put(NoteType.Note1000, 1);
+		map.put(NoteType.Note500, 1);
+		map.put(NoteType.Note100, 1);
 
-		for (int count = 100; count < 2000; count = count+10) {
+		BigDecimal amount = dispenser.deposit(map);
+		amount = new BigDecimal(500);
+		System.out.println("Deposit:" + amount);
+ 		Map<NoteType, Integer> result = dispenser.dispensing(amount);
+
+			System.out.println(result);
+			BigDecimal total = BigDecimal.ZERO;
+			for (Entry<NoteType, Integer> note : result.entrySet()) {
+				BigDecimal totalAmountInNote = note.getKey().getAmount().multiply(new BigDecimal(note.getValue()));
+				total = total.add(totalAmountInNote);
+
+
+
+
+			}
+
+
+			assertEquals(amount, total);
+
+	}
+
+	@Test
+	public void testDispensing() throws MoneyException {
+		Map<NoteType, Integer> map = new HashMap<>();
+		map.put(NoteType.Note1000, Note1000);
+		map.put(NoteType.Note500, Note500);
+		map.put(NoteType.Note100, Note100);
+		map.put(NoteType.Note50, Note50);
+		map.put(NoteType.Note20, Note20);
+
+		BigDecimal amount = dispenser.deposit(map);
+		System.out.println("Deposit:" + amount);
+
+
+		System.out.println(dispenser.getTotalMoney());
+		BigDecimal summary = BigDecimal.ZERO;
+		Map<NoteType, Integer> totalNoteWithdraw = new HashMap<>();
+		for (int count = 110; count < 2000; count = count+10) {
 			System.out.println(count);
-			BigDecimal amount = new BigDecimal(count);
+			amount = new BigDecimal(count);
 
 			Map<NoteType, Integer> result = dispenser.dispensing(amount);
 
@@ -43,10 +81,43 @@ class DispenserTest {
 				BigDecimal totalAmountInNote = note.getKey().getAmount().multiply(new BigDecimal(note.getValue()));
 				total = total.add(totalAmountInNote);
 
+				Integer totalNotes = totalNoteWithdraw.get(note.getKey());
+				if(Objects.isNull(totalNotes)){
+					totalNotes = 0;
+				}
+				totalNotes = totalNotes + note.getValue();
+				totalNoteWithdraw.put(note.getKey(),totalNotes);
+
+
 			}
 
+
+			summary = summary.add(total);
+			System.out.println("s "+total);
+			System.out.println("r "+dispenser.getTotalMoney());
+			//test total withdraw
 			assertEquals(amount, total);
+
+			//test summary notes
+			assertEquals(Note1000,getNote(totalNoteWithdraw,NoteType.Note1000)+getNote(dispenser.currentMoney,NoteType.Note1000));
+			assertEquals(Note500,getNote(totalNoteWithdraw,NoteType.Note500)+getNote(dispenser.currentMoney,NoteType.Note500));
+			assertEquals(Note100,getNote(totalNoteWithdraw,NoteType.Note100)+getNote(dispenser.currentMoney,NoteType.Note100));
+			assertEquals(Note50,getNote(totalNoteWithdraw,NoteType.Note50)+getNote(dispenser.currentMoney,NoteType.Note50));
+			assertEquals(Note20,getNote(totalNoteWithdraw,NoteType.Note20)+getNote(dispenser.currentMoney,NoteType.Note20));
 		}
+
+		System.out.println("summary "+summary);
+		System.out.println("getTotalMoney "+dispenser.getTotalMoney());
+	}
+
+	private int getNote(Map<NoteType, Integer> notes,NoteType noteType){
+		Integer note = notes.get(noteType);
+		if(Objects.isNull(note)){
+			return 0;
+		}
+
+		return note;
+
 
 	}
 
